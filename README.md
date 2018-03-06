299f3c0bf2e1668981b2098174ae23592d366fd6
David Ben-Yaakov0 <david@runningwind.com>
2018-03-06 00:36:11 -0600

    First version

---
+++ README.md
@@ -0,0 +1,5 @@
+# Allen ISD Computer Science Assignment
+# Bubble Sort
+# John Williams
+# Computer Science III, Period 5
+# 2018.03.06
---
+++ main.swift
@@ -0,0 +1,68 @@
+// Allen ISD Computer Science Assignment
+// Bubble Sort
+// John Williams
+// Computer Science III, Period 5
+// 2018.03.06
+
+/********************************************************************************
+As always, READ ALL INSTRUCTIONS!!!
+
+Throughout this lab (and all future labs) PAY CLOSE ATTENTION to the names
+you select for functions, parameters, variables, and constants.
+
+In this lab, you'll be implementing Bubble Sort to sort a series of integers
+and print the results.
+********************************************************************************/
+
+
+
+
+/********************************************************************************
+First, define a "swap" function which accepts three parameters and returns
+nothing. 
+The first parameter is an "inout" array of integers.
+The second parameter is the index to an element in this array.
+The third parameter is the index of another element in this array.
+At the completion of the function, the first element specified will be swapped
+with the second element specified.
+ ********************************************************************************/
+
+
+
+
+/********************************************************************************
+Interactively collect ten integers from the user.
+Be sure to appropriately prompt the user so they'll know what to expect.
+For each integer, let them know how many integers remain to be collected.
+The user must enter the integer on the _same_ line as the prompt.
+Be sure to appropriately handle any invalid input by repeating the prompt.
+ ********************************************************************************/
+
+
+
+
+/********************************************************************************
+Sort the array in ascending order using Bubble Sort and making use of the "swap" 
+function that you defined earlier.  
+Keep track of the total number of passes and the total number of swaps.
+ ********************************************************************************/
+
+
+
+
+/********************************************************************************
+Print the total number of swaps and the total number of passes using string
+interpolation on a single line.
+ ********************************************************************************/
+
+
+
+
+/********************************************************************************
+Print the array in its current (sorted) order on a single line using a comma
+to separate each element.
+ ********************************************************************************/
+
+
+
+
